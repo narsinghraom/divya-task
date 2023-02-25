3feaefdb3c8c5f51812deb13f6d04d688681261b
Narsingh <narsinghraom.dhatriinfo@gmail.com>
2023-02-25 06:28:47 +0530

    Spring boot project created

---
+++ retailer-rewards-api/src/main/java/com/retailer/rewards/RetailerRewardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.retailer.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailerRewardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailerRewardsApiApplication.class, args);
+	}
+
+}
